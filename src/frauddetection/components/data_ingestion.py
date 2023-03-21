5089eec981be26619a38bf70f0a29ff0d577e9f5
Arun Khare <arunvkhare@gmail.com>
2023-03-21 09:04:21 +0530

    added components and entity files

--- src/frauddetection/components/data_ingestion.py
+++ src/frauddetection/components/data_ingestion.py
@@ -1,20 +1,124 @@
 from src.frauddetection.entity.config_entity import DataIngestionConfig
-import sys,os
-from exception import FraudException
-from logger import logging
+from src.exception import FraudException
+from src.logger import logging
+import shutil
+import sys, os
+import pandas as pd
+import tarfile
+from src.frauddetection.entity.artifact_entity import DataIngestionArtifact
+from sklearn.model_selection import train_test_split
+from zipfile import ZipFile
 
 class DataIngestion:
-
-    def __init__(self,data_ingestion_config:DataIngestionConfig ):
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
         try:
-            logging.info(f"{'='*20}Data Ingestion log started.{'='*20} ")
+            logging.info("{=*20} Data ingestion start{=*20}")
             self.data_ingestion_config = data_ingestion_config
+            
         except Exception as e:
-            raise FraudException(e,sys)
+            raise FraudException(e, sys) from e
 
+  
+    def download_payment_data(self,) -> str:
+        try:
+            #extraction remote url to download dataset
+            download_dataset_link = self.data_ingestion_config.dataset_download_url
+
+            #folder location to download file
+            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+            
+            if os.path.exists(tgz_download_dir):
+                os.remove(tgz_download_dir)
+            
+            os.makedirs(tgz_download_dir,exist_ok=True)
+
+            base_filename = "online-payments-fraud-detection-dataset.zip"
+
+            tgz_file_path = os.path.join(tgz_download_dir, base_filename)
+
+            logging.info(f"Downloading file from :[{download_dataset_link}] into :[{tgz_file_path}]")
+            if not os.path.exists(tgz_file_path):
+                os.system('cmd /c "kaggle datasets download -d rupakroy/online-payments-fraud-detection-dataset"')
+                shutil.copy("online-payments-fraud-detection-dataset.zip",tgz_download_dir)
+                logging.info(f"Download file from :[{download_dataset_link}] into [{tgz_file_path}]")
+            else:
+                logging.info(f"dataset file already exist")
+            logging.info(f"File :[{tgz_file_path}] has been downloaded successfully.")
+            return tgz_file_path
+
+        except Exception as e:
+            raise FraudException(e,sys) from e
+
+    def extract_tgz_file(self,tgz_file_path:str):
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+
+            os.makedirs(raw_data_dir,exist_ok=True)
+
+            logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
+            # with tarfile.open(tgz_file_path) as payments_tgz_file_obj:
+            #     payments_tgz_file_obj.extractall(path=raw_data_dir)
+            with ZipFile(tgz_file_path,'r') as zobj:
+                zobj.extractall(path=raw_data_dir)
+            
+            logging.info(f"Extraction completed")
+
+        except Exception as e:
+            raise FraudException(e,sys) from e
+    
+    def split_data_as_train_test(self) -> DataIngestionArtifact:
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+
+            file_name = os.listdir(raw_data_dir)[0]
+
+            payments_filepath = os.path.join(raw_data_dir,file_name)
+
+
+            logging.info(f"Reading csv file: [{payments_filepath}]")
+            payment_data_frame = pd.read_csv(payments_filepath)
+
+            training_set, test_set = train_test_split(payment_data_frame, test_size=0.33, random_state=42)
+
+        
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
+                                            file_name)
+
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
+                                        file_name)
+            
+            if train_file_path is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
+                logging.info(f"Exporting training datset to file: [{train_file_path}]")
+                training_set.to_csv(train_file_path,index=False)
+
+            if test_file_path is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir, exist_ok= True)
+                logging.info(f"Exporting test dataset to file: [{test_file_path}]")
+                test_set.to_csv(test_file_path,index=False)
+            
+
+            data_ingestion_artifact = DataIngestionArtifact(train_file_path=train_file_path,
+                                test_file_path=test_file_path,
+                                is_ingested=True,
+                                message=f"Data ingestion completed successfully."
+                                )
+            logging.info(f"Data Ingestion artifact:[{data_ingestion_artifact}]")
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise FraudException(e,sys) from e
 
     def initiate_data_ingestion(self)-> DataIngestionArtifact:
         try:
-            pass
+            tgz_file_path =  self.download_payment_data()
+            self.extract_tgz_file(tgz_file_path=tgz_file_path)
+            return self.split_data_as_train_test()
         except Exception as e:
             raise FraudException(e,sys) from e
+    
+    def __del__(self):
+        logging.info(f"{'>>'*20}Data Ingestion log completed.{'<<'*20} \n\n")
--- src/frauddetection/utils/utils.py
+++ src/frauddetection/utils/utils.py
@@ -1,7 +1,7 @@
 import yaml
 from src.exception import FraudException
   
-import os
+import os,sys
 from box.exceptions import BoxValueError
 import yaml
 from src import logger
@@ -11,28 +11,31 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import numpy as np
+import dill
+import pandas as pd
+from src.frauddetection.constants import *
 
-
-@ensure_annotations
-def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """reads yaml file and returns
-    Args:
-        path_to_yaml (str): path like input
-    Raises:
-        ValueError: if yaml file is empty
-        e: empty file
-    Returns:
-        ConfigBox: ConfigBox type
-    """
-    try:
-        with open(path_to_yaml) as yaml_file:
-            content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-            return ConfigBox(content)
-    except BoxValueError:
-        raise ValueError("yaml file is empty")
-    except Exception as e:
-        raise e
+# @ensure_annotations
+# def read_yaml_file(file_path: Path) -> ConfigBox:
+#     """reads yaml file and returns
+#     Args:
+#         path_to_yaml (str): path like input
+#     Raises:
+#         ValueError: if yaml file is empty
+#         e: empty file
+#     Returns:
+#         ConfigBox: ConfigBox type
+#     """
+#     try:
+#         with open(path_to_yaml) as yaml_file:
+#             content = yaml.safe_load(yaml_file)
+#             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+#             return ConfigBox(content)
+#     except BoxValueError:
+#         raise ValueError("yaml file is empty")
+#     except Exception as e:
+#         raise e
 
 
 @ensure_annotations
@@ -110,3 +113,102 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path) / 1024)
     return f"~ {size_in_kb} KB"
+
+@ensure_annotations
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise FraudException(e,sys)
+
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Reads a YAML file and returns the contents as a dictionary.
+    file_path: str
+    """
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise FraudException(e,sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise FraudException(e, sys) from e
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise FraudException(e, sys) from e
+
+
+def save_object(file_path:str,obj):
+    """
+    file_path: str
+    obj: Any sort of object
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise FraudException(e,sys) from e
+
+
+def load_object(file_path:str):
+    """
+    file_path: str
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise FraudException(e,sys) from e
+
+
+def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
+    try:
+        datatset_schema = read_yaml_file(schema_file_path)
+        schema = datatset_schema[DATASET_SCHEMA_COLUMNS_KEY]
+        dataframe = pd.read_csv(file_path)
+        error_message = ""
+
+        # for column in dataframe.columns:
+        #     if column in list(schema.keys()):
+        #         dataframe[column].astype(schema[column])
+        #     else:
+        #         error_message = f"{error_message} \nColumn: [{column}] is not in the schema."
+        # if len(error_message) > 0:
+        #     raise Exception(error_message)
+        return dataframe
+
+    except Exception as e:
+        raise FraudException(e,sys) from e
+    
